7e2b09d75b6f3cb22291866ba477b2fd13ec83a8
bakunovgerman <german.bakunov@gmail.com>
2021-07-31 20:30:46 +0700

    complete final

--- app/src/main/java/com/example/bakunov_test_task/NewsActivity.kt
+++ app/src/main/java/com/example/bakunov_test_task/NewsActivity.kt
@@ -9,20 +9,21 @@ import android.view.View.GONE
 import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.constraintlayout.solver.widgets.ConstraintWidget.GONE
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bakunov_test_task.adapters.ItemsAdapter
 import com.example.bakunov_test_task.data.ItemRss
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserException
 import org.xmlpull.v1.XmlPullParserFactory
 import java.io.IOException
 import java.io.InputStream
+import java.lang.Exception
 import java.net.MalformedURLException
 import java.net.URL
+import androidx.lifecycle.lifecycleScope
 
 class NewsActivity : AppCompatActivity() {
 
@@ -37,26 +38,26 @@ class NewsActivity : AppCompatActivity() {
         // забираем адрес из переданного интента
         val address: String? = intent.getStringExtra("address")
         initView()
-        initRV()
         downloadNews(address)
     }
 
+    // инициализация view
     private fun initView() {
         progressbarLayout = findViewById(R.id.progressbarLayout)
+        itemRv = findViewById(R.id.recyclerView)
     }
 
+    // показать progressBar
     private fun showProgressBar(){
         progressbarLayout.visibility = View.VISIBLE
     }
 
+    // скрыть progressBar
     private fun hideProgressBar(){
         progressbarLayout.visibility = View.INVISIBLE
     }
 
-    private fun initRV() {
-        itemRv = findViewById(R.id.recyclerView)
-    }
-
+    // метод для получения inputStream
     private fun getInputStream(url: URL): InputStream?{
         return try {
             url.openConnection().getInputStream()
@@ -65,28 +66,23 @@ class NewsActivity : AppCompatActivity() {
         }
     }
 
+    // метод парсинга XML
     private suspend fun parsingRSS(address: String?): MutableList<ItemRss>?{
-        Log.d("parsing", "start")
         try {
-            Log.d("parsing", "try")
             val itemRssList = mutableListOf<ItemRss>()
             val url = URL(address)
-            Log.d("parsing", "url: ${url.toString()}")
             val factory: XmlPullParserFactory = XmlPullParserFactory.newInstance()
             factory.isNamespaceAware = false
             val xpp: XmlPullParser = factory.newPullParser()
-            xpp.setInput(getInputStream(url), null)
+            xpp.setInput(getInputStream(url), "UTF_8")
             var insideItem: Boolean = false
             var eventType: Int = xpp.eventType
             var itemRss: ItemRss? = null
 
             while(eventType != XmlPullParser.END_DOCUMENT){
-                //Log.d("parsing", "while: ${eventType.toString()}")
                 if (eventType == XmlPullParser.START_TAG){
-                    //Log.d("parsing", "START_TAG")
                     if (xpp.name.equals("item", true)){
                         insideItem = true
-                        //Log.d("parsing", "insideItem true")
                         itemRss = ItemRss()
                     }
                     else if (xpp.name.equals("title", true) && insideItem){
@@ -99,7 +95,7 @@ class NewsActivity : AppCompatActivity() {
                         itemRss?.category = xpp.nextText()
                     }
                     else if (xpp.name.equals("enclosure", true) && insideItem){
-                        itemRss?.imgUrl = xpp.getAttributeValue(1)
+                        itemRss?.imgUrl = xpp.getAttributeValue(XmlPullParser.NO_NAMESPACE, "url")
                     }
                 }
                 else if (eventType == XmlPullParser.END_TAG && xpp.name.equals("item", true)){
@@ -110,30 +106,38 @@ class NewsActivity : AppCompatActivity() {
 
                 eventType = xpp.next()
             }
-            Log.d("parsing", itemRssList.size.toString())
+
             return itemRssList
         } catch (e: MalformedURLException){
-
+            Log.d("exceptionUrl", e.toString())
+        } catch (e: XmlPullParserException){
+            Log.d("exceptionUrl", e.toString())
+        } catch (e: IOException){
+            Log.d("exceptionUrl", e.toString())
         }
         return null
     }
 
+    // корутина для загрузка и сета данных в recyclerView
     private fun downloadNews(address: String?){
         CoroutineScope(Dispatchers.Main).launch {
             showProgressBar()
             val list: MutableList<ItemRss>? = withContext(Dispatchers.IO) { parsingRSS(address) }
-            Log.d("foreach", "size: ${list?.size}")
-            list?.forEach {
-                Log.d("foreach", "category: ${it.category}")
+            if (list != null){
+                itemsRssAdapter = ItemsAdapter(list){
+                    Toast.makeText(this@NewsActivity, it.title, Toast.LENGTH_LONG).show()
+                }
+                itemRv.apply {
+                    adapter = itemsRssAdapter
+                    layoutManager = LinearLayoutManager(this@NewsActivity)
+                }
+                hideProgressBar()
             }
-            itemsRssAdapter = ItemsAdapter(list){
-                Toast.makeText(this@NewsActivity, it.title, Toast.LENGTH_LONG).show()
+            else{
+                Toast.makeText(this@NewsActivity, "Ошибка!", Toast.LENGTH_LONG).show()
+                finish()
             }
-            itemRv.apply {
-                adapter = itemsRssAdapter
-                layoutManager = LinearLayoutManager(this@NewsActivity)
-            }
-            hideProgressBar()
         }
     }
+
 }
